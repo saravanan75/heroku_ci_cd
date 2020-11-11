ef6bc1ac56361e608c53587898955d0f6f926c86
saravanan75 <43355335+saravanan75@users.noreply.github.com>
2020-11-11 14:42:00 -0600

    Update HerokuCiApplication.java
    Added a commit

--- src/main/java/com/example/sharu/pluralsight/heroku_ci/HerokuCiApplication.java
+++ src/main/java/com/example/sharu/pluralsight/heroku_ci/HerokuCiApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HerokuCiApplication {
 	@GetMapping("/hellops")
+	//Code Changed
 	public String helloHeroku()
 	{
 		return "Hello from pluralsight course";
