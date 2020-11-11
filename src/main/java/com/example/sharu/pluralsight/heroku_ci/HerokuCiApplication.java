8fa98f4d0c30430f4ef3cb76c40533d0ef936126
Saravanan Dhandapani <saravanan.dhandapani@gmail.com>
2020-11-11 14:20:57 -0600

    Initial Commit

---
+++ src/main/java/com/example/sharu/pluralsight/heroku_ci/HerokuCiApplication.java
@@ -0,0 +1,21 @@
+package com.example.sharu.pluralsight.heroku_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HerokuCiApplication {
+	@GetMapping("/hellops")
+	public String helloHeroku()
+	{
+		return "Hello from pluralsight course";
+	}
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HerokuCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sharu/pluralsight/heroku_ci/HerokuCiApplicationTests.java
@@ -0,0 +1,35 @@
+package com.example.sharu.pluralsight.heroku_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class HerokuCiApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
+	@Autowired
+	private MockMvc mockMvc;
+	@LocalServerPort
+	private int port;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hellops",
+				String.class)).contains("Hello from pluralsight course");
+	}
+
+
+
+}
